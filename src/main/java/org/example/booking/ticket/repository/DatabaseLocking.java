68ec97ddb73b48d458d8a241a40f381eb215658e
duyvk <imkhuongduy@gmail.com>
2024-09-15 15:41:39 +0700

    add logging and fix bugs about concurrency

--- src/main/java/org/example/booking/ticket/repository/DatabaseLocking.java
+++ src/main/java/org/example/booking/ticket/repository/DatabaseLocking.java
@@ -1,29 +1,56 @@
 package org.example.booking.ticket.repository;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+@Slf4j
 public class DatabaseLocking {
     private static final Map<Object, ReentrantLock> tableLocks = new ConcurrentHashMap<>();
-    
-    public static synchronized void acquireTableLock(Object key) {
-        if (!tableLocks.containsKey(key)) {
-            tableLocks.put(key, new ReentrantLock());
-        }
-        ReentrantLock lock = tableLocks.get(key);
-        if (!lock.isHeldByCurrentThread()) {
-            lock.lock();
+    private static final ReentrantLock methodLock = new ReentrantLock();
+    private static final Map<Object, Condition> conditions = new ConcurrentHashMap<>();
+
+    public static void acquireTableLock(Object key) {
+        log.debug("Acquiring lock for key {} in thread {}", key, Thread.currentThread().getId());
+        methodLock.lock();
+        try {
+            if (!tableLocks.containsKey(key)) {
+                tableLocks.put(key, new ReentrantLock());
+                conditions.put(key, methodLock.newCondition());
+            }
+            ReentrantLock lock = tableLocks.get(key);
+            Condition condition = conditions.get(key);
+            while (!lock.tryLock(0, TimeUnit.SECONDS)) {
+                condition.await();
+            }
+            log.debug("Lock for key {} in thread {} held {} after acquiring", key, Thread.currentThread().getId(), lock.getHoldCount());
+            log.debug("Acquired lock for key {} in thread {}", key, Thread.currentThread().getId());
+        } catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		} finally {
+            methodLock.unlock();
         }
     }
 
-    public static synchronized void releaseTableLock(Object key) {
-        if (!tableLocks.containsKey(key)) {
-            throw new IllegalStateException("No such lock for key: " + key);
-        }
-        ReentrantLock lock = tableLocks.get(key);
-        if (lock.isHeldByCurrentThread()) {
+    public static void releaseTableLock(Object key) {
+        log.debug("Releasing lock for key {} in thread {}", key, Thread.currentThread().getId());
+        methodLock.lock();
+        try {
+            if (!tableLocks.containsKey(key)) {
+                throw new IllegalStateException("No such lock for key: " + key);
+            }
+            ReentrantLock lock = tableLocks.get(key);
+            Condition condition = conditions.get(key);
             lock.unlock();
+            condition.signalAll();
+            log.debug("Lock for key {} in thread {} held {} after releasing", key, Thread.currentThread().getId(), lock.getHoldCount());
+            log.debug("Released lock for key {} in thread {}", key, Thread.currentThread().getId());
+        } finally {
+            methodLock.unlock();
         }
     }
 }
--- src/main/java/org/example/booking/ticket/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/booking/ticket/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package org.example.booking.ticket.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.booking.ticket.entity.Ticket;
 import org.example.booking.ticket.entity.TicketType;
 import org.example.booking.ticket.entity.User;
@@ -18,75 +19,81 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
-    private final TicketRepository ticketRepository;
-    private final TicketTypeRepository ticketTypeRepository;
-    private final MapperFactory factory;
+	private final UserRepository userRepository;
+	private final TicketRepository ticketRepository;
+	private final TicketTypeRepository ticketTypeRepository;
+	private final MapperFactory factory;
 
-    public UserServiceImpl(UserRepository userRepository, TicketRepository ticketRepository, TicketTypeRepository ticketTypeRepository,
-                           MapperFactory factory) {
-        this.userRepository = userRepository;
-        this.ticketRepository = ticketRepository;
-        this.ticketTypeRepository = ticketTypeRepository;
-        this.factory = factory;
-    }
+	public UserServiceImpl(UserRepository userRepository, TicketRepository ticketRepository, TicketTypeRepository ticketTypeRepository,
+						   MapperFactory factory) {
+		this.userRepository = userRepository;
+		this.ticketRepository = ticketRepository;
+		this.ticketTypeRepository = ticketTypeRepository;
+		this.factory = factory;
+	}
 
-    @Override
-    public List<UserResponse> getUsers() {
-        return factory.getMapper(User.class, UserResponse.class).map(userRepository.findAll());
-    }
+	@Override
+	public List<UserResponse> getUsers() {
+		return factory.getMapper(User.class, UserResponse.class).map(userRepository.findAll());
+	}
 
-    @Override
-    public void save(UserCreateRequest userCreateRequest) {
-        User user = factory.getMapper(UserCreateRequest.class, User.class).map(userCreateRequest);
-        userRepository.save(user);
-    }
+	@Override
+	public void save(UserCreateRequest userCreateRequest) {
+		User user = factory.getMapper(UserCreateRequest.class, User.class).map(userCreateRequest);
+		userRepository.save(user);
+	}
 
-    @Override
-    public UserResponse getUserById(UUID id) {
-        return factory.getMapper(User.class, UserResponse.class).map(userRepository.findById(id));
-    }
+	@Override
+	public UserResponse getUserById(UUID id) {
+		return factory.getMapper(User.class, UserResponse.class).map(userRepository.findById(id));
+	}
 
-    @Override
-    public void buyTicket(UUID ticketId, UUID userId) {
-        DatabaseLocking.acquireTableLock(User.class);
-        DatabaseLocking.acquireTableLock(Ticket.class);
-        DatabaseLocking.acquireTableLock(TicketType.class);
+	@Override
+	public void buyTicket(UUID ticketId, UUID userId) {
+		log.info("User {} buying ticket {}", userId, ticketId);
+		DatabaseLocking.acquireTableLock(User.class);
+		DatabaseLocking.acquireTableLock(Ticket.class);
+		DatabaseLocking.acquireTableLock(TicketType.class);
 
-        try {
-            User user = userRepository.findById(userId);
-            if (user == null) {
-                throw new DataInvalidException("User not found");
-            }
+		try {
+			User user = userRepository.findById(userId);
+			if (user == null) {
+				throw new DataInvalidException("User not found");
+			}
 
-            Ticket ticket = ticketRepository.findById(ticketId);
-            if (ticket == null) {
-                throw new DataInvalidException("Ticket not found");
-            }
+			Ticket ticket = ticketRepository.findById(ticketId);
+			if (ticket == null) {
+				throw new DataInvalidException("Ticket not found");
+			}
 
-            if (ticket.getOwnerId() != null) {
-                throw new DataInvalidException("Ticket is already bought");
-            }
+			if (ticket.getOwnerId() != null) {
+				throw new DataInvalidException("Ticket is already bought");
+			}
 
-            TicketType ticketType = ticketTypeRepository.findById(ticket.getTicketTypeId());
-            if (ticketType == null) {
-                throw new DataInvalidException("Ticket type not found");
-            }
+			TicketType ticketType = ticketTypeRepository.findById(ticket.getTicketTypeId());
+			if (ticketType == null) {
+				throw new DataInvalidException("Ticket type not found");
+			}
 
-            if (ticketType.getPrice().compareTo(user.getBalance()) > 0) {
-                throw new DataInvalidException("Not enough balance");
-            }
+			if (ticketType.getPrice().compareTo(user.getBalance()) > 0) {
+				throw new DataInvalidException("Not enough balance");
+			}
 
-            user.setBalance(user.getBalance().subtract(ticketType.getPrice()));
-            ticket.setOwnerId(user.getId());
+			user.setBalance(user.getBalance().subtract(ticketType.getPrice()));
+			ticket.setOwnerId(user.getId());
 
-            userRepository.update(user);
-            ticketRepository.update(ticket);
-        } finally {
-            DatabaseLocking.releaseTableLock(User.class);
-            DatabaseLocking.releaseTableLock(Ticket.class);
-            DatabaseLocking.releaseTableLock(TicketType.class);
-        }
-    }
+			userRepository.update(user);
+			ticketRepository.update(ticket);
+			log.info("User {} bought ticket {}", userId, ticketId);
+		} catch (RuntimeException e) {
+			log.error("User {} bought ticket {} failed: {}", userId, ticketId, e.getMessage());
+			throw e;
+		} finally {
+			DatabaseLocking.releaseTableLock(User.class);
+			DatabaseLocking.releaseTableLock(Ticket.class);
+			DatabaseLocking.releaseTableLock(TicketType.class);
+		}
+	}
 }
